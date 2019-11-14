ff4e676151e81c389b51605f7c26a01bf02691c3
mediateri <ntawukuriryayomeddy1@gmail.com>
2019-11-14 13:45:50 +0000

    meddy

--- clock.rb
+++ clock.rb
@@ -1,12 +1,10 @@
+require 'slack-notifier'
 require 'clockwork'
-include Clockwork
-
-handler do |job|
-  puts "Running #{job}"
+require 'active_support/time'
+module Clockwork
+  handler do |send|
+    notifier = Slack::Notifier.new('https://hooks.slack.com/services/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')
+    notifier.ping('Hello')
+  end
+  every(3.seconds, 'send')
 end
-
-every(10.seconds, 'frequent.job')
-every(3.minutes, 'less.frequent.job')
-every(1.hour, 'hourly.job')
-
-every(1.day, 'midnight.job', :at => '00:00')
